a3ef11269b9f82335fff60b2fb1548636eaf5700
Eulaliane <32363620+Eulaliane@users.noreply.github.com>
2017-10-16 18:26:15 -0200

    Update L4_exercicio3.hs

--- Lista4/L4_exercicio3.hs
+++ Lista4/L4_exercicio3.hs
@@ -1,4 +1,17 @@
-diagonalSecundaria :: [[Int]] -> [[Int]]
-diagonalSecundaria [x] = _
-diagonalSecundaria (x:xs) = juntar (tail x) (diagonalSecundaria xs)
+type Vetor = [Int]
+type Matriz = [[Int]]
 
+numFilas :: Matriz -> Int
+numFilas = fst . snd . bounds
+
+numColumnas:: Matriz -> Int
+numColumnas = snd . snd . bounds
+
+diagonalSec :: Matriz -> Vector
+diagonalSec p = array (1,n) [(i,p!(i,n+1-i)) | i <- [1..n]]
+    where n = min (numFilas p) (numColumnas p)
+
+main = do
+  print(diagonalSec [[1,2,3],
+                     [4,5,6],
+                     [3,2,1]])
