0f5c28098bb4dca9192286d90dd64609a956f7e0
Eulaliane <32363620+Eulaliane@users.noreply.github.com>
2017-11-18 22:40:56 -0200

    Delete L1_exercicio10.hs

--- Lista1/L1_exercicio10.hs
+++
@@ -1,7 +0,0 @@
-ex10 :: ([Int],[Int])
-xs = [ano | ano <- [1..2017], (ano `rem` 400 == 0) || ((ano `rem` 4 == 0) && (ano `rem` 100 /= 0))]
-n = (length xs) `div` 2
-ex10 = (take n xs, drop n xs)
-
-main = do
-  print(ex10)
