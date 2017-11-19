d836df24a0272f56125533034fdfb133aeb4ed34
Eulaliane <32363620+Eulaliane@users.noreply.github.com>
2017-11-18 22:43:33 -0200

    Delete L3_exercicio6.hs

--- Lista3/L3_exercicio6.hs
+++
@@ -1,9 +0,0 @@
-collatz :: Int -> Int
-collatz x 
-  | x < 1 = error "Numero menor do que 1"
-  | x == 1 = 1
-  | x `mod` 2 == 0 = x `div`2
-  | otherwise = (3*x+1)`div`2
-
-main = do
-  print(collatz 15)
