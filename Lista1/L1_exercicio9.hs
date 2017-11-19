9427756791996025a6651dab2183f87961badc10
Eulaliane <32363620+Eulaliane@users.noreply.github.com>
2017-11-18 22:41:54 -0200

    Delete L1_exercicio9.hs

--- Lista1/L1_exercicio9.hs
+++
@@ -1,9 +0,0 @@
-ex9 :: [Int]
-
-lista = [ano | ano <- [1..2017], (ano `rem` 400 == 0) || ((ano `rem` 4 == 0) && (ano `rem` 100 /= 0))]
-
-ex9 = drop (length lista - 10) lista
-
-
-main = do
-  print(ex9)
