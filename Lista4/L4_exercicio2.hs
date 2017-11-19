dfb45094f9171bbac830d8a07c8df5a2c1d06f0e
Eulaliane <32363620+Eulaliane@users.noreply.github.com>
2017-11-18 22:42:39 -0200

    Delete L4_exercicio2.hs

--- Lista4/L4_exercicio2.hs
+++
@@ -1,8 +0,0 @@
-diagonalPrincipal :: [[Int]] -> [Int]
-diagonalPrincipal xs = zipWith (!!) xs [0..]
-
-somaDiagonalPrincipal :: [[Int]] -> Int
-somaDiagonalPrincipal xs = sum(diagonalPrincipal xs)
-
-main = do
-  print(somaDiagonalPrincipal [[1,2,3],[4,5,6],[3,2,1]])
